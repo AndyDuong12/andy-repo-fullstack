31f0db38b701ca5e3a29ae811a21a18ecd52c13e
Andy <andyduong03@gmail.com>
2025-01-29 21:13:18 -0800

    02-node-routing.js completed

--- hw2/02-node-routing.js
+++ hw2/02-node-routing.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 const port = process.env.PORT || 5001;
 
 // http://localhost:5001/welcome should return a status code 200 with a welcome message of your choice in html format
@@ -13,17 +13,67 @@ const port = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
   const routes = [
-    'welcome',
-    'redirect',
-    'redirected',
-    'cache',
-    'cookie',
-    'other',
+    "welcome",
+    "redirect",
+    "redirected",
+    "cache",
+    "cookie",
+    "other",
   ];
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('Node Routing Exercise');
-  res.end();
+  // /
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Node Routing Exercise");
+    res.end();
+  }
+
+  // /welcome
+  else if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>welcome<h1>");
+    res.end();
+  }
+
+  // /redirect
+  else if (req.url === "/redirect") {
+    res.writeHead(302, { Location: "/redirected" });
+    res.end();
+  }
+
+  // /redirected
+  else if (req.url === "/redirected") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<p>redirected from redirect<p>");
+    res.end();
+  }
+
+  // /cache
+  else if (req.url === "/cache") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+      "Cache-Control": "max-age=86400",
+    });
+    res.write("this resource was cached");
+    res.end();
+  }
+
+  // /cookie
+  else if (req.url === "/cookie") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+      "Set-Cookie": "hello=world",
+    });
+    res.write("cookies... yummm");
+    res.end();
+  }
+
+  // Other routes
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("404: Page not found");
+    res.end();
+  }
 });
 
 server.listen(port, () => {
